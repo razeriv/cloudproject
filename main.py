f5f6b5731d1b4f36cf4c9aa8bf6642735dc75153
razeriv <128141016+razeriv@users.noreply.github.com>
2026-03-14 00:56:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,17 @@
 import threading
-from bot import start_bot  # Импортируйте функцию старта бота
-from app import app     # Импортируйте Flask-приложение
+from bot import start_bot
+from app import app
 
 def run_flask_server():
-    """Функция для запуска Flask приложения"""
     print("Starting Flask server...")
-    # Отключаем debug, чтобы избежать конфликта сигналов
     app.run(debug=False, use_reloader=False, host='0.0.0.0', port=5000)
 
 def run_bot():
-    """Функция для запуска бота"""
     print("Starting Bot...")
     start_bot()
 
 if __name__ == "__main__":
-    # Запускаем Flask сервер в отдельном потоке
     flask_thread = threading.Thread(target=run_flask_server)
     flask_thread.start()
 
-    # Запускаем бот в главном потоке
     run_bot()
